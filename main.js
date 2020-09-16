c46f77f974cd98a7a618ba0de718886532bc3bd1
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-09-15 22:25:01 -0500

    Creación clase persona

---
+++ main.js
@@ -0,0 +1,13 @@
+class Persona {
+
+  constructor(nombre, apellido, edad, ID, sexo, peso, altura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.ID = ID;
+    this.sexo = sexo;
+    this.peso = peso;
+    this.altura = altura;
+  }
+}
+console.log(Persona);
