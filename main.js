aa05038cdbf136b0ce1b7c05c9de5d71152cac51
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-09-19 15:20:23 -0500

    nueva persona con métodos, IMC, mayor de edad, id

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 class Persona {
 
-  constructor(nombre, apellido, edad, ID, sexo, peso, altura) {
+  constructor(nombre, apellido, edad, sexo, peso, altura) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
-    this.ID = ID;
     this.sexo = sexo;
     this.peso = peso;
-    this.altura = altura;
+    this.altura = altura / 100;
+    this.id = this.generarID();
   }
+
+  calcularIMC() {
+    return this.peso / (this.altura * this.altura);
+  }
+  
+  esMayorDeEdad() {
+    return this.edad > 18;
+
+  }
+
+  calcularAnio() {
+    let date = new Date();
+    let anio = date.getFullYear();
+    return anio - this.edad;
+  }
+  generarID() {
+    this.id = `${this.nombre.substring(0, 3)}${this.apellido.substring(0,3)}${this.calcularAnio()}${this.sexo}`;
+        return this.id;
+  }
+  
+
 }
-console.log(Persona);
+
+const valeria = new Persona("Valeria", "Quintanilla", 34, "femenino", 53, 1.6);
+console.log("El IMC es: " + valeria.calcularIMC());
+console.log("es mayor de edad: " + valeria.esMayorDeEdad());
+console.log("el id es: " + valeria.generarID());
+//console.log("el id es: " + this.id);
